5e1ee0e2b841e9d98dc9b43ad61a55541ca9fded
= <user.email>
2020-07-21 17:20:45 +0100

    Copy recordlinkage files from prefect-toolkit
    link-records should be a standalone library

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+# base requirements
+install_requires = open("requirements.txt").read().strip().split("\n")
+
+extras = {
+    'viz': ['graphviz>=0.14']
+}
+extras["all_extras"] = sum(extras.values(), [])
+
+setup(
+    name='link_records',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    version='0.0.1',
+    author='rdmolony',
+    author_email='rowan.molony@codema.ie',
+    description='',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/rdmolony/link-records',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=install_requires,
+    python_requires='>=3.6',
+)
+
