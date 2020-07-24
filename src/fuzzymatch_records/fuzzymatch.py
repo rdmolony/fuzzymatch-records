d73c24d3d9a20c6f04f575e1519b25845f8f239f
Rowan Molony <rowan.molony@codema.ie>
2020-07-24 15:47:44 +0100

    Extend fuzzymatch for left_on and right_on
    In drem I need left_on and right_on for fuzzymatching
    2 datasets with different column names ...

--- src/fuzzymatch_records/fuzzymatch.py
+++ src/fuzzymatch_records/fuzzymatch.py
@@ -1,49 +1,134 @@
-import re
-from typing import List, Tuple
-from pathlib import Path
-from logging import Logger
 import logging
 import os
+import re
 import sys
+from logging import Logger
+from pathlib import Path
+from typing import List, Tuple, Union, Sequence
 
-import pandas as pd
+import icontract
 import numpy as np
+import pandas as pd
+import recordlinkage
+from fuzzymatch_records.clean_columns import clean_fuzzy_column, clean_fuzzy_columns
 from string_grouper import (
-    match_most_similar,
-    match_strings,
     StringGrouper,
     group_similar_strings,
+    match_most_similar,
+    match_strings,
 )
-import recordlinkage
-
-from fuzzymatch_records.clean_columns import clean_fuzzy_column, clean_fuzzy_columns
 
 
+@icontract.require(
+    lambda on_fuzzy, on_fuzzy_left, on_fuzzy_right: (on_fuzzy is not None)
+    or ((on_fuzzy_left is not None) & (on_fuzzy_right is not None))
+)
+@icontract.require(lambda min_similarities: min_similarities is not None)
+@icontract.require(
+    lambda left, right: isinstance(left, pd.DataFrame) & isinstance(right, pd.DataFrame)
+)
 def fuzzymatch_dataframes(
     left: pd.DataFrame,
     right: pd.DataFrame,
-    fuzzy_column_properties: List[Tuple[str, float]],
+    on_fuzzy: List[str] = None,
+    on_fuzzy_left: List[str] = None,
+    on_fuzzy_right: List[str] = None,
+    min_similarities: List[float] = None,
 ) -> pd.DataFrame:
+    """Appends fuzzymatched columns from 'left' to 'right' DataFrames
+    according to the min_similarity specified for each column.
 
-    on_fuzzy, min_similarities = zip(*fuzzy_column_properties)
+    Example
+    -------
+    fuzzymatch_dataframes(left, right, ['company_name', 'address'], [0.9, 0.6])
+    0.9 represents the minimum allowable (cossine) similarity between the
+    'company_name' columns in the 'left' and 'right' DataFrames to define
+    the match_most_similar a success
 
-    left = left.copy().pipe(clean_fuzzy_columns, on_fuzzy)
-    right = right.copy().pipe(clean_fuzzy_columns, on_fuzzy)
+    See https://github.com/Bergvca/string_grouper for more information on 
+    fuzzy matching
 
-    for fuzzy_column, min_similarity in zip(on_fuzzy, min_similarities):
+    Parameters
+    ----------
+    left : pd.DataFrame
+    right : pd.DataFrame
+    on_fuzzy : List[str]
+    on_fuzzy_left : List[str]
+    on_fuzzy_right : List[str]
+    min_similarities: List[float]
 
-        right[fuzzy_column + "_fuzzymatched"] = match_most_similar(
-            left[fuzzy_column], right[fuzzy_column], min_similarity=min_similarity
-        )
+    Returns
+    -------
+    pd.DataFrame
+    """
+    if on_fuzzy:
+
+        left = left.copy().pipe(clean_fuzzy_columns, on_fuzzy)
+        right = right.copy().pipe(clean_fuzzy_columns, on_fuzzy)
+
+        for fuzzy_column, min_similarity in zip(on_fuzzy, min_similarities):
+
+            right[fuzzy_column + "_fuzzymatched"] = match_most_similar(
+                left[fuzzy_column], right[fuzzy_column], min_similarity=min_similarity
+            )
+
+    else:
+
+        left = left.copy().pipe(clean_fuzzy_columns, on_fuzzy_left)
+        right = right.copy().pipe(clean_fuzzy_columns, on_fuzzy_right)
+
+        for (fuzzy_column_left, fuzzy_column_right, min_similarity) in zip(
+            on_fuzzy_left, on_fuzzy_right, min_similarities,
+        ):
+
+            right[
+                fuzzy_column_right + "_fuzzymatched_to_" + fuzzy_column_left
+            ] = match_most_similar(
+                left[fuzzy_column_left],
+                right[fuzzy_column_right],
+                min_similarity=min_similarity,
+            )
 
     return right
 
 
+@icontract.require(
+    lambda on, left_on, right_on: (on is not None)
+    or ((left_on is not None) & (right_on is not None))
+)
+@icontract.require(lambda min_similarity: min_similarity is not None)
+@icontract.require(
+    lambda min_similarity: isinstance(min_similarity, float) or min_similarity == 0
+)
+@icontract.require(
+    lambda left, right: (isinstance(left, pd.Series) & isinstance(right, pd.Series))
+    or (isinstance(left, pd.DataFrame) & isinstance(right, pd.DataFrame))
+)
 def calculate_fuzzymatches_for_min_similarity(
-    left: pd.DataFrame, right: pd.DataFrame, column: str, min_similarity: float,
+    left: Union[pd.DataFrame, pd.Series],
+    right: Union[pd.DataFrame, pd.Series],
+    on: str = None,
+    left_on: str = None,
+    right_on: str = None,
+    min_similarity: float = None,
 ) -> pd.DataFrame:
 
-    left_clean = left[column].drop_duplicates().pipe(clean_fuzzy_column)
-    right_clean = right[column].drop_duplicates().pipe(clean_fuzzy_column)
+    if isinstance(left, pd.Series) and isinstance(right, pd.Series):
+        left_clean = left.drop_duplicates().pipe(clean_fuzzy_column)
+        right_clean = right.drop_duplicates().pipe(clean_fuzzy_column)
 
-    return match_strings(left_clean, right_clean, min_similarity=min_similarity)
+    elif on is not None:
+        left_clean = left[on].drop_duplicates().pipe(clean_fuzzy_column)
+        right_clean = right[on].drop_duplicates().pipe(clean_fuzzy_column)
+
+    elif (left_on is not None) and (right_on is not None):
+        left_clean = left[left_on].drop_duplicates().pipe(clean_fuzzy_column)
+        right_clean = right[right_on].drop_duplicates().pipe(clean_fuzzy_column)
+
+    else:
+        raise ValueError("Unexpected condition...")
+
+    match = match_strings(left_clean, right_clean, min_similarity=min_similarity)
+
+    return match
+
--- tests/test_fuzzymatch.py
+++ tests/test_fuzzymatch.py
@@ -1,48 +1,44 @@
-from typing import Dict
 from pathlib import Path
+from typing import Any, Dict
 
+import icontract
 import pandas as pd
-from pandas.testing import assert_frame_equal
 import pytest
+from fuzzymatch_records.fuzzymatch import (
+    fuzzymatch_dataframes,
+    calculate_fuzzymatches_for_min_similarity,
+)
+from pandas.testing import assert_frame_equal
 from string_grouper import match_strings
-from fuzzymatch_records.fuzzymatch import fuzzymatch_dataframes
 
 CWD = Path(__file__).parent
 DATA = CWD / "data"
 
 
-# @pytest.fixture(params=["CompaniesFuzzyMatches", "AddressesFuzzyMatches"])
-# def fuzzymatched_data(request) -> Dict[str, pd.DataFrame]:
+@pytest.fixture(params=["CompaniesFuzzyMatches", "AddressesFuzzyMatches"])
+def fuzzymatched_data(request) -> Dict[str, pd.DataFrame]:
 
-#     return pd.read_excel(DATA / f"{request.param}.ods", sheet_name=None, engine="odf")
+    return pd.read_excel(DATA / f"{request.param}.ods", sheet_name=None, engine="odf")
 
 
-# @pytest.mark.parametrize(
-#     "input_sheet, output_sheet",
-#     [
-#         ("fuzzymatches", "fuzzymatches_result"),
-#         ("not_fuzzymatches", "not_fuzzymatches_result"),
-#     ],
-# )
-# def test_match_most_similar_for_data(
-#     fuzzymatched_data, input_sheet, output_sheet
-# ) -> None:
+@pytest.mark.parametrize("min_similarity", [0, 0.5, 1])
+@pytest.mark.parametrize(
+    "input_sheet, output_sheet",
+    [
+        ("fuzzymatches", "fuzzymatches_result"),
+        ("not_fuzzymatches", "not_fuzzymatches_result"),
+    ],
+)
+def test_calculate_fuzzymatches_for_min_similarity_succeeds(
+    fuzzymatched_data, input_sheet, output_sheet, min_similarity
+) -> None:
 
-#     input = fuzzymatched_data[input_sheet]
-#     left, right = input["left"], input["right"]
-#     # expected_output = fuzzymatched_data[output_sheet]
+    input = fuzzymatched_data[input_sheet]
+    left, right = input["left"], input["right"]
 
-#     output = match_strings(left, right, min_similarity=0)
-#     import ipdb
-
-#     ipdb.set_trace()
-#     assert_frame_equal(
-#         output,
-#         expected_output,
-#         check_less_precise=True,
-#         check_dtype=False,
-#         check_like=True,
-#     )
+    calculate_fuzzymatches_for_min_similarity(
+        left, right, left_on="left", right_on="right", min_similarity=0
+    )
 
 
 @pytest.fixture
@@ -51,25 +47,64 @@ def example_dataset() -> Dict[str, pd.DataFrame]:
     return pd.read_excel(DATA / "ExampleDataset.ods", sheet_name=None, engine="odf")
 
 
-# def test_fuzzymatch_dataframes(example_dataset) -> None:
+@pytest.mark.parametrize("min_similarity", [(0, 0), (0.5, 0.5), (1, 1)])
+def test_fuzzymatch_dataframes_on_data(example_dataset, min_similarity) -> None:
 
-#     left = example_dataset["left"]
-#     right = example_dataset["right"]
+    left = example_dataset["left"]
+    right = example_dataset["right"]
 
-#     pb_name_min_similarity = 0.8
-#     location_min_similarity = 0.7
-#     expected_output = example_dataset["matched_pb0_8_loc0_7"]
+    fuzzymatch_dataframes(
+        left, right, on_fuzzy=["PB Name", "Location"], min_similarities=min_similarity,
+    )
 
-#     output = fuzzymatch_dataframes(
-#         left,
-#         right,
-#         [("PB Name", pb_name_min_similarity), ("Location", location_min_similarity)],
-#     )
 
-#     assert_frame_equal(
-#         output,
-#         expected_output,
-#         check_less_precise=True,
-#         check_dtype=False,
-#         check_like=True,
-#     )
+@pytest.fixture
+def breaking_inputs() -> Dict[str, Any]:
+
+    left = pd.DataFrame([1, 2, 3])
+    right = pd.DataFrame([1, 2, 3])
+    on_fuzzy = ["blah"]
+    min_similarity = [0.1]
+    on_fuzzy_left = ["blah"]
+    on_fuzzy_right = ["blah"]
+
+    return {
+        "empty_on_fuzzy": [left, right, None, min_similarity, None, None],
+        "only_on_fuzzy_left": [left, right, None, min_similarity, on_fuzzy_left, None],
+        "empty_min_similarity": [left, right, on_fuzzy, None, None, None],
+        "string_min_similarity": [left, right, on_fuzzy, "imastring", None, None],
+        "not_a_dataframe": [1, 2, on_fuzzy, min_similarity, None, None],
+    }
+
+
+@pytest.mark.parametrize(
+    "key",
+    [
+        "empty_on_fuzzy",
+        "only_on_fuzzy_left",
+        "string_min_similarity",
+        "empty_min_similarity",
+        "not_a_dataframe",
+    ],
+)
+def test_fuzzymatch_dataframes_raises_error(breaking_inputs, key) -> None:
+
+    (
+        left,
+        right,
+        on_fuzzy,
+        on_fuzzy_left,
+        on_fuzzy_right,
+        min_similarities,
+    ) = breaking_inputs[key]
+
+    with pytest.raises(icontract.ViolationError):
+        fuzzymatch_dataframes(
+            left,
+            right,
+            on_fuzzy=on_fuzzy,
+            on_fuzzy_left=on_fuzzy_left,
+            on_fuzzy_right=on_fuzzy_right,
+            min_similarity=min_similarities,
+        )
+
