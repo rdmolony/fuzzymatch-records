70fb2ee045bbfddd4711f9684863e15b8ba97c2d
Rowan Molony <rowan.molony@codema.ie>
2020-08-04 14:53:13 +0100

    Single Capture Group Extract Pcodes & Address Numbers  (#3)
    * Extract numeric pcodes only & all address numbers
    
    So Co. Dublin not caught as sometimes address entered is
    Dublin 7 Co. Dublin
    
    Extract all address numbers rather than only if
    preceded by Block, Unit, Floor etc. as this less
    specific filter will catch more numbers...
    
    * Provide option to specify new column names & add docstrings
    
    * Drop address column prior to replacing it!
    
    If don't drop the address column pandas creates a
    new column with an identical name

--- src/fuzzymatch_records/parse_addresses.py
+++ src/fuzzymatch_records/parse_addresses.py
@@ -7,6 +7,36 @@ from IPython.core.debugger import set_trace
 def _extract_dublin_postcodes(series: pd.Series) -> pd.Series:
 
     return series.str.extract(pat=r"(dublin \d+\w?)", flags=IGNORECASE)[0]
+<<<<<<< HEAD
+
+
+def extract_dublin_postcodes(
+    df: pd.DataFrame, address_column: str, postcode_column: str = "extracted_postcodes",
+) -> pd.DataFrame:
+    """Extracts numeric county names (Dublin 1, Dublin 2 etc) from the
+    specified address column.  Won't extract Dublin County addresses
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+    address_column : str
+        Name of address column
+    postcode_column: str, optional
+        Name of new postcode column, by default "extracted_postcodes" 
+
+    Returns
+    -------
+    pd.DataFrame
+    """
+    return df.assign(
+        **{postcode_column: df[address_column].pipe(_extract_dublin_postcodes)}
+    )
+
+
+def _remove_dublin_postcodes(series: pd.Series) -> pd.Series:
+
+    return series.str.replace(pat=r"(dublin \d+\w?)", repl="", flags=IGNORECASE)
+=======
 
 
 def extract_dublin_postcodes(df: pd.DataFrame, column: str) -> pd.DataFrame:
@@ -24,16 +54,36 @@ def extract_dublin_postcodes(df: pd.DataFrame, column: str) -> pd.DataFrame:
     pd.DataFrame
     """
     return df.assign(extracted_postcodes=df[column].pipe(_extract_dublin_postcodes))
+>>>>>>> fde24b279ac88119a9810e18cd861a74d8e45a90
 
 
-def _remove_dublin_postcodes(series: pd.Series) -> pd.Series:
+def remove_dublin_postcodes(df: pd.DataFrame, address_column: str) -> pd.DataFrame:
+    """Removes Dublin postcodes in-place from specified address column
 
+<<<<<<< HEAD
+    Parameters
+    ----------
+    df : pd.DataFrame
+    address_column : str
+        Name of address column in dataframe
+=======
     return series.str.replace(pat=r"(dublin \d+\w?)", repl="", flags=IGNORECASE)
+>>>>>>> fde24b279ac88119a9810e18cd861a74d8e45a90
 
+    Returns
+    -------
+    pd.DataFrame
+    """
 
-def remove_dublin_postcodes(df: pd.DataFrame, column: str) -> pd.DataFrame:
+    df = df.copy().drop(columns=address_column)
 
-    return df.assign(**{column: df[column].pipe(_remove_dublin_postcodes).astype(str)})
+    return df.assign(
+        **{
+            address_column: df[address_column]
+            .pipe(_remove_dublin_postcodes)
+            .astype(str)
+        }
+    )
 
 
 def _extract_address_numbers(series: pd.Series) -> pd.Series:
@@ -41,6 +91,20 @@ def _extract_address_numbers(series: pd.Series) -> pd.Series:
     import ipdb
 
     ipdb.set_trace()
+<<<<<<< HEAD
+=======
+
+    pattern = """
+    (
+        \w*         # (optional) Starts with letters (ex: M4)
+        \d+         # All numeric characters 
+        [/-]?       # (optional) '-' or '/' (ex: 19/...)
+        \d*         # (optional) second group of numbers (ex: 19/20)
+        \w*         # (optional) Ends with letters (1st)
+    )"""
+
+    return series.str.extract(pat=pattern, flags=IGNORECASE | VERBOSE)[0]
+>>>>>>> fde24b279ac88119a9810e18cd861a74d8e45a90
 
     pattern = """
     (
@@ -54,6 +118,26 @@ def _extract_address_numbers(series: pd.Series) -> pd.Series:
     return series.str.extract(pat=pattern, flags=IGNORECASE | VERBOSE)[0]
 
 
-def extract_address_numbers(df: pd.DataFrame, column: str) -> pd.DataFrame:
+def extract_address_numbers(
+    df: pd.DataFrame,
+    address_column: str,
+    address_number_column: str = "address_numbers",
+) -> pd.DataFrame:
+    """Extracts address numbers into a new column
 
-    return df.assign(address_numbers=df[column].pipe(_extract_address_numbers))
+    Parameters
+    ----------
+    df : pd.DataFrame
+    address_column : str
+        Name of address column
+    address_number_column : str, optional
+        New column containing address numbers, by default "address_numbers"
+
+    Returns
+    -------
+    pd.DataFrame
+    """
+
+    return df.assign(
+        **{address_number_column: df[address_column].pipe(_extract_address_numbers)}
+    )
